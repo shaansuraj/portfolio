4716a26d88952d3656469d7c1c90a7372393bb5b
Suraj Sahu <90745657+shaansuraj@users.noreply.github.com>
2022-10-09 19:43:10 +0530

    Update ScrollToTop.js

--- src/Components/ScrollToTop.js
+++ src/Components/ScrollToTop.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 class ScrollToTop extends Component {
-  componentDidUpdate(prevProps) {
+//   componentDidUpdate(prevProps) {
+  window.onbeforeunload = function (prevProps){
     if (this.props.location !== prevProps.location) {
       window.scrollTo(0, 0);
     }
