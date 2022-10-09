6fdcfb92d81c4a833f677afe834a2d3f59536f5a
Suraj Sahu <90745657+shaansuraj@users.noreply.github.com>
2022-10-09 19:03:35 +0530

    Update TechStack.js

--- src/Components/TechStack/TechStack.js
+++ src/Components/TechStack/TechStack.js
@@ -14,8 +14,8 @@ import {  SiFirebase } from "react-icons/si";
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        href = "https://cplusplus.com/doc/"
+      <Col xs={4} md={2} className="tech-icons"  href = "https://cplusplus.com/doc/" >
+       
         <CgCPlusPlus />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
