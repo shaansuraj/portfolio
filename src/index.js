e69c2be13b0fa95e91045375341c21b63576ab06
Suraj Sahu <90745657+shaansuraj@users.noreply.github.com>
2022-10-09 10:49:16 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const smoothDirect = () => {
+    window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+}
+
 
 ReactDOM.render(
   <React.StrictMode>
