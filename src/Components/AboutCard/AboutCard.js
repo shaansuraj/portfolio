8856b1d972cdc1ef400cdf40271d3e77ec9fa2ce
Suraj Sahu <90745657+shaansuraj@users.noreply.github.com>
2022-10-09 18:50:54 +0530

    Update AboutCard.js

--- src/Components/AboutCard/AboutCard.js
+++ src/Components/AboutCard/AboutCard.js
@@ -8,18 +8,18 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, My Self <span className="purple">Suraj Sahu </span>
-            I am a 2nd Year BTech Student from <span className="purple"> Bhubaneswar, India.</span>
+            😉😉😉 Hi Everyone, My Self <span className="purple">Suraj Sahu </span>
+            I am a 2nd Year BTech Student from <span className="purple"> Bhubaneswar, India </span>
             <br />
             
-            <br />I am pursuing my Bachelor's degree in Computer Science and Engineering from Institute of Technical Education and Research <br /> <br />
+            <br /> 🥱🥱🥱 I am pursuing my Bachelor's degree in Computer Science and Engineering from Institute of Technical Education and Research <br /> <br />
            <br />
             
-            ⚡ I am a MERN Stack Developer with hands-on experience in Cloud Computing with AWS. I have knowledge of DSA, OS, ML and Automation.
+            😊 I am a MERN Stack Developer with hands-on experience in Cloud Computing with AWS. I have knowledge of DSA, OS, ML and Automation.
             <br />
            <br />
             <br />
-            ⚡ Apart from this, I am a AWS Cloud Practiner Trainee and have done DevOPs with AWS.
+            😏 Apart from this, I am a AWS Cloud Practioner Trainee and have done DevOPs with AWS.
  
           </p>
           
