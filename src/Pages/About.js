5427ba0e28a5644a0f272da14d69d5e9cfb789eb
Suraj Sahu <surajsahu9658@gmail.com>
2023-02-19 10:46:10 +0530

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -44,25 +44,7 @@ function About() {
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
-            <div itemscope itemtype='http://schema.org/Person' class='fiverr-seller-widget' style='display: inline-block;'>
-     <a itemprop='url' href=https://www.fiverr.com/shaansuraj rel="nofollow" target="_blank" style='display: inline-block;'>
-        <div class='fiverr-seller-content' id='fiverr-seller-widget-content-820a8ebf-0b73-4b23-bd4b-eaff148cbb97' itemprop='contentURL' style='display: none;'></div>
-        <div id='fiverr-widget-seller-data' style='display: none;'>
-            <div itemprop='name' >shaansuraj</div>
-            <div itemscope itemtype='http://schema.org/Organization'><span itemprop='name'>Fiverr</span></div>
-            <div itemprop='jobtitle'>Seller</div>
-            <div itemprop='description'>From the basics to the core, I am a self-taught full-stack web developer with hands-on experience in frontend and backend development and a hardcore programmer with a never-ending thirst to do better. I use the MERN Stack as my tech stack to develop websites and web applications. For building android application, I use Android Studio and Java. I am an AWS Solution Architecture Trainee.
-My projects include the following:
-1) Clones using MERN Stack: Instagram, Tinder, WhatsApp
-2) Real-time Chat Web Application using PHP
-3) Portfolio using ReactJS
-4) Automation Script for Linkedin Connections
-</div>
-        </div>
-    </a>
-</div>
-
-<script id='fiverr-seller-widget-script-820a8ebf-0b73-4b23-bd4b-eaff148cbb97' src='https://widgets.fiverr.com/api/v1/seller/shaansuraj?widget_id=820a8ebf-0b73-4b23-bd4b-eaff148cbb97' data-config='{"category_name":"Programming \u0026 Tech"}' async='true' defer='true'></script>
+            
       </Container>
     </Container>
   );
